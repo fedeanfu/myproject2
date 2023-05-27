e76d22b29735d1a0ff656389bce9a0d7289b36d9
feder <federica.anfuso@gmail.com>
2023-05-27 11:24:51 +0200

    ho salutato gli italiani e aggiunto una nuova classe

--- Test.java
+++ Test.java
@@ -1,6 +1,10 @@
+import java.sql.SQLOutput;
+
 public class Test{
     public static void main(String[] args) {
         System.out.println("Hello from my project");
         System.out.println("Ciao italiani");
+
+        System.out.println("---------------------");
     }
 }
